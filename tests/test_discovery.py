2ffbdc8ac2f4b6bfec1e26ada12a2d388fbebc20
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-07 21:56:50 +0100

    Add simple test for announce/discover cycle
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,4 +1,18 @@
 
+from myr.discovery import announce, discover
 
-def test_discovery():
-    pass
+
+def test_announce():
+    tasks = {
+        'myr.billstack.something': {
+            'signature': {},
+            'routing': {'queue': 'billstack'},
+        },
+        'myr.invoicing.something': {
+            'signature': {},
+            'routing': {'queue': 'invoicing'},
+        }
+    }
+    announce(tasks)
+    discovered_tasks = discover()
+    assert tasks == discovered_tasks
